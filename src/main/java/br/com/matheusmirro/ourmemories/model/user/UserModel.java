95a57d2322fffcb5e5c501dd7f5b16473c1160a1
Matheus Alves <mirro123@outlook.com>
2023-10-16 11:06:28 -0300

    feat: model user finished

--- src/main/java/br/com/matheusmirro/ourmemories/model/user/UserModel.java
+++ src/main/java/br/com/matheusmirro/ourmemories/model/user/UserModel.java
@@ -1,5 +1,33 @@
 package br.com.matheusmirro.ourmemories.model.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+
+    private UUID id;
+
+    private String name;
+    private String lastname;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
