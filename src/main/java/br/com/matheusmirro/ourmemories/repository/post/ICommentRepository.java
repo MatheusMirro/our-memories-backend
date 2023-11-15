d2ec36b9b069d8d379e85e4619e304d39857242f
Matheus Alves <mirro123@outlook.com>
2023-11-15 02:06:02 -0300

    feat: repository to find a comment

---
+++ src/main/java/br/com/matheusmirro/ourmemories/repository/post/ICommentRepository.java
@@ -0,0 +1,13 @@
+package br.com.matheusmirro.ourmemories.repository.post;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.matheusmirro.ourmemories.model.post.CommentModel;
+
+@Repository
+public interface ICommentRepository extends JpaRepository<CommentModel, UUID> {
+    List<CommentModel> findAllByPost_Id(UUID postId);
+
+}
