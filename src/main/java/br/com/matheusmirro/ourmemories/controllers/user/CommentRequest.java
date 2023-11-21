9174e17df0c26f738fe776a6670e0c0e3549d059
Matheus Alves <mirro123@outlook.com>
2023-11-21 02:07:30 -0300

    refactor: notation to generate a default constructor

--- src/main/java/br/com/matheusmirro/ourmemories/controllers/user/CommentRequest.java
+++ src/main/java/br/com/matheusmirro/ourmemories/controllers/user/CommentRequest.java
@@ -1,8 +1,10 @@
 package br.com.matheusmirro.ourmemories.controllers.user;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CommentRequest {
     private String text;
 
