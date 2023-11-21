10eb96581496fe4b397e3c6278e69c90d7e04516
Matheus Alves <mirro123@outlook.com>
2023-11-21 02:45:50 -0300

    refactor: refactoring like model based in sql

--- src/main/java/br/com/matheusmirro/ourmemories/model/user/LikeModel.java
+++ src/main/java/br/com/matheusmirro/ourmemories/model/user/LikeModel.java
@@ -1,23 +1,26 @@
 package br.com.matheusmirro.ourmemories.model.user;
 
-import java.util.UUID;
-
 import br.com.matheusmirro.ourmemories.model.post.PostModel;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Entity
 @Data
-@Table(name = "likes")
+@Table(name = "tb_likes")
 public class LikeModel {
     @Id
-    @GeneratedValue(generator = "UUID")
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "like_id")
+    private UUID likeId;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -26,4 +29,8 @@ public class LikeModel {
     @ManyToOne
     @JoinColumn(name = "post_id")
     private PostModel post;
+
+    @Column(name = "total_likes")
+    private int totalLikes;
+
 }
