34fa38c7e63244e04cdba5ff4b93bd6d13c45c56
Matheus Alves <mirro123@outlook.com>
2023-11-22 15:14:34 -0300

    refactor: passing the logic from the controller to the service

--- src/main/java/br/com/matheusmirro/ourmemories/controllers/post/PostController.java
+++ src/main/java/br/com/matheusmirro/ourmemories/controllers/post/PostController.java
@@ -1,8 +1,5 @@
 package br.com.matheusmirro.ourmemories.controllers.post;
 
-import java.time.LocalDateTime;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,53 +8,21 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import br.com.matheusmirro.ourmemories.infra.security.TokenService;
-import br.com.matheusmirro.ourmemories.model.post.PostModel;
-import br.com.matheusmirro.ourmemories.model.user.UserModel;
-import br.com.matheusmirro.ourmemories.repository.post.IPostRepository;
+import br.com.matheusmirro.ourmemories.service.post.PostService;
 
 @RestController
 @RequestMapping("/post")
 public class PostController {
-    @Autowired
-    private IPostRepository postRepository;
+    private final PostService postService;
 
-    @Autowired
-    TokenService tokenService;
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 
     @PostMapping("/picture")
     public ResponseEntity<String> uploadingPost(@RequestPart("uploadingFiles") MultipartFile[] uploadingFiles,
             Authentication authentication) {
 
-        try {
-            // identify user by token getting his name
-            var authenticatedUser = (UserModel) authentication.getPrincipal();
-            for (MultipartFile file : uploadingFiles) {
-                if (!isValidImage(file)) {
-                    return ResponseEntity.badRequest().body("Arquivo inválido: " + file.getOriginalFilename());
-                }
-
-                PostModel postModel = new PostModel();
-                postModel.setFile_name(file.getOriginalFilename());
-                postModel.setFile_type(file.getContentType());
-                postModel.setFile_size(file.getSize());
-                postModel.setUpload_date(LocalDateTime.now());
-                postModel.setUser(authenticatedUser);
-                postRepository.save(postModel);
-            }
-        } catch (Exception e) {
-            var error = e.getMessage();
-            System.out.println(error);
-        }
-        return ResponseEntity.accepted().body("Upload feito com sucesso!");
-    }
-
-    private boolean isValidImage(MultipartFile file) {
-        if (file == null || file.isEmpty()) {
-            System.out.println("Arquivo vazio ou nulo.");
-            return false;
-        }
-
-        return true;
+        return postService.uploadingPost(uploadingFiles, authentication);
     }
 }
--- src/main/java/br/com/matheusmirro/ourmemories/service/post/PostService.java
+++ src/main/java/br/com/matheusmirro/ourmemories/service/post/PostService.java
@@ -1,8 +1,57 @@
 package br.com.matheusmirro.ourmemories.service.post;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.matheusmirro.ourmemories.model.post.PostModel;
+import br.com.matheusmirro.ourmemories.model.user.UserModel;
+import br.com.matheusmirro.ourmemories.repository.post.IPostRepository;
 
 @Service
 public class PostService {
+
+    @Autowired
+    private final IPostRepository postRepository;
+
+    public PostService(IPostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public ResponseEntity<String> uploadingPost(MultipartFile[] uploadinFiles, Authentication authentication) {
+        try {
+            // identify user by token getting his name
+            var authenticatedUser = (UserModel) authentication.getPrincipal();
+            for (MultipartFile file : uploadinFiles) {
+                if (!isValidImage(file)) {
+                    return ResponseEntity.badRequest().body("Arquivo inválido: " + file.getOriginalFilename());
+                }
+
+                PostModel postModel = new PostModel();
+                postModel.setFile_name(file.getOriginalFilename());
+                postModel.setFile_type(file.getContentType());
+                postModel.setFile_size(file.getSize());
+                postModel.setUpload_date(LocalDateTime.now());
+                postModel.setUser(authenticatedUser);
+                postRepository.save(postModel);
+            }
+        } catch (Exception e) {
+            var error = e.getMessage();
+            System.out.println(error);
+        }
+        return ResponseEntity.accepted().body("Upload feito com sucesso!");
+    }
+
+    private boolean isValidImage(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            System.out.println("Arquivo vazio ou nulo.");
+            return false;
+        }
+        return true;
+    }
 
 }
