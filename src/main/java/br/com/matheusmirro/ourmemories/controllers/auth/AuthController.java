f9e8355820fcdbe3adbf1777a9d2337084d4df87
Matheus Alves <mirro123@outlook.com>
2023-12-07 10:16:43 -0300

    Update of the authcontroller with the correct returns for the login route + the construction of the decoding route + validation of the token

--- src/main/java/br/com/matheusmirro/ourmemories/controllers/auth/AuthController.java
+++ src/main/java/br/com/matheusmirro/ourmemories/controllers/auth/AuthController.java
@@ -1,17 +1,26 @@
 package br.com.matheusmirro.ourmemories.controllers.auth;
 
+import java.util.Date;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 import br.com.matheusmirro.ourmemories.auth.domain.user.AuthenticationDTO;
-import br.com.matheusmirro.ourmemories.auth.domain.user.LoginResponseDTO;
+import br.com.matheusmirro.ourmemories.controllers.user.TokenRequest;
 import br.com.matheusmirro.ourmemories.infra.security.TokenService;
 import br.com.matheusmirro.ourmemories.model.user.UserModel;
+import br.com.matheusmirro.ourmemories.repository.user.IUserRepository;
 
 @RestController
 public class AuthController {
@@ -20,15 +29,56 @@ public class AuthController {
     TokenService tokenService;
 
     @Autowired
+    IUserRepository userRepository;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
 
+    @CrossOrigin
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO data) {
-
+    public ResponseEntity<Object> login(@RequestBody AuthenticationDTO data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.username(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
         var token = tokenService.generateToken((UserModel) auth.getPrincipal());
 
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+        var userResponse = new HashMap<>();
+        userResponse.put("user", data.username());
+
+        // Create a main response object to encapsulate user and token responses
+        var mainResponse = new HashMap<>();
+        mainResponse.put("user", userResponse);
+        mainResponse.put("token", token);
+
+        return ResponseEntity.ok(mainResponse);
+    }
+
+    @CrossOrigin
+    @PostMapping("/api/auth/validate")
+    public ResponseEntity<Object> validateToken(@RequestBody TokenRequest request, AuthenticationDTO data) {
+
+        String tokenToDecode = request.getToken();
+
+        try {
+
+            DecodedJWT decodedToken = JWT.decode(tokenToDecode);
+
+            var decodedResponse = new HashMap<>();
+            decodedResponse.put("decoded", decodedToken);
+
+            if (decodedToken.getExpiresAt().before(new Date())) {
+                return ResponseEntity.badRequest().body("Token expired");
+            }
+
+            var userResponse = new HashMap<>();
+            userResponse.put("user", decodedToken.getSubject());
+
+            var mainResponse = new HashMap<>();
+            mainResponse.put("user", userResponse);
+            mainResponse.put("decoded", decodedToken.getClaims());
+
+            return ResponseEntity.ok().body(mainResponse);
+        } catch (JWTDecodeException e) {
+            return ResponseEntity.badRequest().body("Invalid token");
+        }
     }
 }
