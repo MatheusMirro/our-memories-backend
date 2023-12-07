06ffddf9ecc987a5866629bcd056cc5be34951fb
Matheus Alves <mirro123@outlook.com>
2023-12-07 10:25:01 -0300

    creation of the file containing all post success strings

---
+++ src/main/java/br/com/matheusmirro/ourmemories/messages/success/PostSuccess.java
@@ -0,0 +1,6 @@
+package br.com.matheusmirro.ourmemories.messages.success;
+
+public class PostSuccess {
+    public static final String COMMENT_RECEIVED = "Comentario recebido!";
+    public static final String UPLOAD_SUCCESSFULY = "Postagem realizado com sucesso!";
+}
